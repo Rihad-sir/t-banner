6a8e4f1edb0e2e2ead69d687f61b21687b00f918
Rihad-sir <88466421+Rihad-sir@users.noreply.github.com>
2021-08-05 06:23:53 +0530

    Create T-banner.sh

---
+++ T-banner.sh
@@ -0,0 +1,88 @@
+<html>
+
+<body>
+
+    <body style="background-color:#00ffff" />
+
+<head>
+
+  <title> nesses cyber ™ </title>
+
+</head>
+
+<body>
+
+  <div style="background-color: #00ff00;"/><u><center><i><p><h2><txt style="color: red"/> NESSES CYBER SECURITY™ </h2></i></center></u></p>
+
+  <center><p><a href="https://althafrihad.wixsite.com/nesesscybersecurityt"> www.ness🌐escyberteam.com🔎 </a></p></center/></b>
+
+</div/>
+
+</head>
+
+<body>
+
+ <b><center><p> hello world. </p></center/>
+
+<head>
+
+     <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+</head>
+
+<body>
+
+       <font style="color: #808000" /> NESSES CBR™ </h1></font>
+
+       <img src="/storage/emulated/999/WhatsApp/Media/WhatsApp images/IMG-20210711-WA0036.jpg" />
+
+<head>
+
+<i><h><ul><div style="background-color: #ff0000" ><p> ABOUT </p></div></h></ul></i>
+
+       <p>NESSUS CYBER SECURITY ™ - NESSUS cyber team are working for justice and world cyber security.they can change the world cyber imagination.they are working for develop the cyber and educational Field.</p>
+
+<p>
+
+  <div style="background-color: black" /><txt style="color: white" /> <center><i> FIELD </p></center>
+
+<p>1-  Cyber Security -/ethical hacking team.</p>
+
+   <p> 2- App / web Developing </p>
+
+<p>3- Technology Teachment</p>
+
+</div>
+
+<u><i><P><font style="color: blue" /> 👣CONTACT ON NESSES CYBER SECURITY ™ ☎️</p></i></font></u>
+
+</head>
+
+</body>
+
+<p><p>
+
+<hr>
+
+ YouTube    =  <div style="background-color: red"> <a href="https://youtube.com/channel/UCTii8kyUd5s1ekCmjcGi43w" /><font color="White"><center> SUBSCRIBE ❤️ </p></font></a></div>
+
+<hr>
+
+ INSTAGRAM   = <div style="background-color: blue"> <a href="https://www.instagram.com/_ri_ha_d_?r=nametag" ><font color="white"><b><center> FOLLOW </p></div></font></center></a>
+
+ INSTAGRAM   = <div style="background-color: blue"> <a href="https://www.instagram.com/anonymous_atf?r=nametag"><font color="white"><b><center> FOLLOW </P></div></font></center></a>
+
+<hr>
+
+ GOOGLE 🌐 =   <div style="background-color: Lime"> <a href="https://althafrihad.wixsite.com/nesesscybersecurityt"><font color="blue"><center> SEARCH🔍 </p></div></font></center></a>
+
+<hr>
+
+</hr>
+
+    <h3><center><div style="background-color: #ff00ff"><font style="color: white"> THANKS FOR YOUR SUPPORTING</h3><p><div><center> 
+
+    <p>credited by nesses cyber security ™
+
+</p>
+
